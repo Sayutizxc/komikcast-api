8ffb08936ef911c3a4ce1087712a8acbcbffad48
Muhammad Sayuti <sayutizxc@gmail.com>
2020-12-09 16:59:30 +0800

    added next and previous

--- scraper/chapter_images.js
+++ scraper/chapter_images.js
@@ -2,15 +2,23 @@ const cheerio = require('cheerio');
 const axios = require('axios');
 
 module.exports = async function chapterImages(url) {
+  const komik = {};
   const img = [];
   try {
     await axios.get(url).then((response) => {
       const $ = cheerio.load(response.data);
-      const data = $('#readerarea img');
-      data.each((_index, element) => {
+      const data = $('.maincontent');
+      komik.now = url;
+      komik.previous =
+        data.find('.nextprev > a:contains("Previous Chapter")').attr('href') ||
+        '';
+      komik.next =
+        data.find('.nextprev > a:contains("Next Chapter")').attr('href') || '';
+      data.find('#readerarea img').each((_index, element) => {
         const $img = $(element);
         if ($img.attr('src') !== '') img.push($img.attr('src'));
       });
+      komik.images = img;
     });
   } catch (e) {
     return {
@@ -18,5 +26,5 @@ module.exports = async function chapterImages(url) {
       message: e.response.statusText,
     };
   }
-  return { images: img };
+  return komik;
 };
